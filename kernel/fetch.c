c5b7f9f7f67ad595acb5b914b5caac8e3c3ef826
toma331 <arthom.koval@gmail.com>
2025-09-30 18:02:21 +0300

    fetch update

--- kernel/fetch.c
+++ kernel/fetch.c
@@ -2,14 +2,13 @@
 #include "headers/screen.h"
 
 void fetch() {
-    print("   ____                 \n");
-    print("  | __ )  ___  ___  ___ \n");
-    print("  |  _ \\ / _ \\/ __|/ _ \\\n");
-    print("  | |_) | (_) \\__ \\  __/\n");
-    print("  |____/ \\___/|___/\\___|\n");
     print("\n");
-    print(" bossyOS v0.1 (toy kernel)\n");
-    print(" VGA Text Mode 80x25\n");
-    print(" Author: Artem Koval :)\n");
+    print("   /\\_/\\      bossyOS v0.1\n");
+    print("  ( o.o )     Kernel: 32-bit ELF\n");
+    print("   > ^ <      Console: VGA 80x25\n");
+    print("              Graphics: text mode\n");
+    print("\n");
+    print("   A cute kitty welcomes you! =^._.^=   "); 
+    put_char('\n');
     print("\n");
 }
